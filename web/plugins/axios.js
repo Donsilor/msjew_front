d645a216f4a4ca5f00710ef004a688d4fa644ed4
gaopeng <763429951@qq.com>
2020-01-15 14:18:12 +0800

    fix

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -12,11 +12,11 @@ export default function({ $axios, store }) {
 
   $axios.onResponse(res => {
     const data = res.data || {}   
-    const accessToken = localStorage.getItem('accessToken') || ''
-	if(accessToken == '' && store.state.token) {
-		store.dispatch('logout')
-		return 
-	}
+    //const accessToken = localStorage.getItem('accessToken')
+	//if(accessToken == '' && store.state.token) {
+	//	store.dispatch('logout')
+	//	return 
+	//}
     if (data.hasOwnProperty('code')) {
       if (data.code == 200) {
         return Promise.resolve(
@@ -26,12 +26,12 @@ export default function({ $axios, store }) {
       } else {
         if (data.code == 401) {
           console.log('is 401')		  
-		  if(accessToken == '' || accessToken == store.state.token) {
-			store.dispatch('logout')			
-		  }else {
+		  //if(accessToken == '' || accessToken == store.state.token) {
+			//store.dispatch('logout')			
+		  //}else {
 			//localStorage.setItem('accessToken',accessToken)  
-			window.location.reload() 
-		  }          
+			//window.location.reload() 
+		  //}          
         }else{
           return Promise.reject(new Error(data.message|| 'something error'))
         }
