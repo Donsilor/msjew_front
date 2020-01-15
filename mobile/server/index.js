a6e95f858fad19f28e7fd1bdf004b04829283a23
donsilor <390807392@qq.com>
2020-01-15 09:41:31 +0800

    updata

--- mobile/server/index.js
+++ mobile/server/index.js
@@ -12,8 +12,8 @@ async function start() {
   const nuxt = new Nuxt(config)
 
 
-  const { host, port } = nuxt.options.server
-  // const { host, port } = { host: '0.0.0.0', port: '3000' }
+  // const { host, port } = nuxt.options.server
+  const { host, port } = { host: '0.0.0.0', port: '3000' }
 
 
   // Build only in dev mode
