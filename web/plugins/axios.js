6473d2045d0bae388cfdaa165d577ae7439d15d7
gaopeng <763429951@qq.com>
2020-01-15 20:44:51 +0800

    fix

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -1,52 +1,51 @@
 export default function ({
-  $axios,
-  store
+    $axios,
+    store
 }) {
-  $axios.onRequest(config => {
+    $axios.onRequest(config => {
 
-    config.headers['x-api-key'] = store.state.token || ''
-    config.headers['x-api-currency'] = store.state.coin || ''
-    config.headers['x-api-language'] = store.state.language || ''
-    // config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    return config
-  })
+        config.headers['x-api-key'] = store.state.token || ''
+        config.headers['x-api-currency'] = store.state.coin || ''
+        config.headers['x-api-language'] = store.state.language || ''
+        // config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        return config
+    })
 
 
 
-  $axios.onResponse(res => {
-    const data = res.data || {}
+    $axios.onResponse(res => {
+        const data = res.data || {}
 
-    if (process.server == false) {
-      let accessToken = localStorage.getItem('accessToken')
-      if (!accessToken && store.state.token) {
-        store.dispatch('logout')
-        window.location.href = '/login'
-      }
-    }
-    if (data.hasOwnProperty('code')) {
-      if (data.code == 200) {
-        return Promise.resolve(![undefined].includes(data) ? data : null)
-        //return Promise.resolve(data.data || null)
-      } else if (data.code == 401) {
         if (process.server == false) {
-          let accessToken = localStorage.getItem('accessToken')
-          if (!accessToken || accessToken == store.state.token) {
-            store.dispatch('logout')
-            window.location.href = '/login'
-          } else {
-            store.commit('setToken', accessToken)
-            window.location.reload()
-          }
+            let accessToken = localStorage.getItem('accessToken')
+            if (!accessToken && store.state.token) {
+                store.dispatch('logout')
+                window.location.href = '/login'
+            }
         }
-      } else {
-        return Promise.reject(new Error(data.message || 'something error'))
-      }
-    } else {
-      return Promise.resolve(data || null)
-    }
-  })
-  $axios.onError(error => {
-    // console.log('$axios.onError===>', error)
-    return Promise.reject(error)
-  })
+        if (data.hasOwnProperty('code')) {
+            if (data.code == 200) {
+                return Promise.resolve(![undefined].includes(data) ? data : null)
+                //return Promise.resolve(data.data || null)
+            } else if (data.code == 401) {
+                if (process.server == false) {
+                    let accessToken = localStorage.getItem('accessToken')
+                    if (!accessToken || accessToken == store.state.token) {
+                        store.dispatch('logout')
+                        window.location.href = '/login'
+                    } else {
+                        window.location.reload()
+                    }
+                }
+            } else {
+                return Promise.reject(new Error(data.message || 'something error'))
+            }
+        } else {
+            return Promise.resolve(data || null)
+        }
+    })
+    $axios.onError(error => {
+        // console.log('$axios.onError===>', error)
+        return Promise.reject(error)
+    })
 }
