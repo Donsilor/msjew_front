72201ec320cec92bf5cc2ceceb22dab73eb5f12d
yzz <2419349086@qq.com>
2020-02-18 12:38:35 +0800

    aaa

--- mobile/middleware/pcToWap.js
+++ mobile/middleware/pcToWap.js
@@ -67,10 +67,10 @@ function queryParams (data) {
     // let host = 'http://localhost:8318';
 
     // 正式环境
-    let host = 'https://www.bddco.com';
+    // let host = 'https://www.bddco.com';
 
     // 测试环境
-    // let host = 'https://www.bdd.bddia.com';
+    let host = 'https://www.bdd.bddia.com';
 
     const toWapUrl = path => {
       if(path === '/undefined') {
