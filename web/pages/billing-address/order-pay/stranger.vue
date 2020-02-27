1bd6c33f0368c6524cd5fb3e04b598d6fffaaf80
yzz <2419349086@qq.com>
2020-02-27 18:32:47 +0800

    cccc

--- web/pages/billing-address/order-pay/stranger.vue
+++ web/pages/billing-address/order-pay/stranger.vue
@@ -1925,7 +1925,7 @@ export default {
           goodsCartList:json,
           tradeType:'pc',
           coinType:this.$store.state.coin,
-          returnUrl:'https://bddco.com/complete-payment?order_sn={order_sn}'  //http://localhost:8318  http://www.bdd.bddia.com
+          returnUrl:'https://www.bddco.com/complete-payment?order_sn={order_sn}'  //http://localhost:8318  http://www.bdd.bddia.com
         }
       })
         .then(res => {
