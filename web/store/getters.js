7bfa75307234f9c7735bc04c9781b28ac4a71971
gaopeng <peng.gao@bddco.com>
2020-03-04 12:17:06 +0800

    fix 当前域名

--- web/store/getters.js
+++ web/store/getters.js
@@ -66,5 +66,18 @@ export default {
     // console.log('用户信息用户信息用户信息用户信息')
     const userInfo = state.userInfo
     return userInfo
+  },
+
+  //当前网址域名
+  baseUrl() {
+    let host = window.location.host;
+    if((/bddco\.com/).test(host)) {
+       //正式
+       host = 'https://www.bddco.com'
+    }else{
+       //测试
+       host = 'http://www.bdd.bddia.com'
+    }
+    return host 
   }
 }
