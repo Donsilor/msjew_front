63cfb378d2077c5c9d25178be27d86a6604045ba
yzz <2419349086@qq.com>
2020-03-11 21:41:10 +0800

    支付3

--- web/pages/complete-paySuccess/state/success.vue
+++ web/pages/complete-paySuccess/state/success.vue
@@ -371,9 +371,9 @@ export default {
           // console.log("order_sn",res)
           this.data2 = res.data
           // http://localhost:8318/complete-payment?order_sn=BDD202002254136556&success=true&paymentId=PAYID-LZKNA5Y2RG00076G1872113M&token=EC-9LP10841H1659180J&PayerID=ZMUBN8MYV9Q5N
-          // setTimeout(() => {
-          //   this.$router.push({path: "/"}); // 强制切换当前路由 path
-          // }, 5000);
+          setTimeout(() => {
+            this.$router.push({path: "/"}); // 强制切换当前路由 path
+          }, 5000);
           // console.log("wwwww",this.data)
         })
         .catch(err => {
