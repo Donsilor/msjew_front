8ce109f8138715b04bd9fe126cb48f561617e219
gaopeng <peng.gao@bddco.com>
2020-03-13 14:43:20 +0800

    fix pc和移动头部

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -9,7 +9,7 @@ export default function ({
         config.headers['x-api-language'] = store.state.language || 'en_US'
         config.headers['x-api-area'] = store.state.areaId || '' // 当前所属地区
         config.headers['x-api-server'] = process.server ? 1 : 0 //是否是服务器端请求
-        config.headers['x-api-platform'] = 'pc'
+        config.headers['x-api-platform'] = 1 //1=>PC端
         // config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
         return config
     })
