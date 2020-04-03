ee853be0d4d053465a4ca6c3f5f9d10e7423a384
donsilor <390807392@qq.com>
2020-04-03 18:16:00 +0800

    updata

---
+++ mobile/pages/news/news-detail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="back" @click="goback">返回</div>
+    <div class="title">文章标题</div>
+    <div class="describe">这是一篇文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章这是一篇是一文章这是一篇文章</div>
+    <div class="detail">文章内容</div>
+  </div>
+</template>
+
+<script>
+  export default{
+    methods:{
+      goback(){
+        history.go(-1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container{
+    position: relative;
+    padding: 0 20px 40px;
+    min-height: 300px;
+  }
+  .back{
+    width: 40px;
+    height: 24px;
+    background-color: sandybrown;
+    border-radius: 2px;
+    text-align: center;
+    line-height: 24px;
+    font-size: 12px;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  .title{
+    width: 100%;
+    height: 60px;
+    line-height: 78px;
+    font-size: 18px;
+    text-align: center;
+    color: #333;
+  }
+  .describe{
+    text-align: left;
+    font-size: 12px;
+    color: #999;
+    line-height: 18px;
+  }
+  .detail{
+    margin-top: 20px;
+    text-align: left;
+    font-size: 14px;
+  }
+</style>
--- web/pageComponents/news/list.vue
+++ web/pageComponents/news/list.vue
@@ -48,11 +48,7 @@
       </div>
 
       <div class="page-box">
-        <el-pagination
-          @current-change=""
-          :current-page="currentPage4"
-          :page-size="page_size"
-          layout="total, prev, pager, next, jumper"
+        <el-pagination @current-change="" :current-page="currentPage4" :page-size="page_size" layout="total, prev, pager, next, jumper"
           :total="totalCount">
         </el-pagination>
       </div>
@@ -61,105 +57,119 @@
 </template>
 
 <script>
-import Layout from '@/pageComponents/news/news-classify.vue'
-const lang = 'policies.international'
-export default {
-  components: {
-    Layout
-  },
-  data() {
-    return {
-      lang,
-      currentPage4: 1,
-      page_size: 2,
-      totalCount: 10
-    }
-  },
-  methods: {
-    go(url) {
-      this.$router.push({
-        path: url
-      })
+  import Layout from '@/pageComponents/news/news-classify.vue'
+  const lang = 'policies.international'
+  export default {
+    components: {
+      Layout
     },
-    handleCurrentChange(){
-      console.log(1111)
+    data() {
+      return {
+        lang,
+        currentPage4: 1,
+        page_size: 2,
+        totalCount: 10
+      }
+    },
+    methods: {
+      go(url) {
+        this.$router.push({
+          path: url
+        })
+      },
+      handleCurrentChange() {
+        console.log(1111)
+      }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
-.content {
-  padding-left: 60px;
-  padding-right: 60px;
-  border: 1px solid rgba(215, 215, 215, 1);
-}
+  a:link,
+  a:visited,
+  a:hover,
+  a:active {
+    color: inherit;
+  }
 
-.article-title{
-  height: 90px;
-  line-height: 100px;
-  font-size: 28px;
-  color: #8b766c;
-  border-bottom: 1px solid #8B766C;
-  padding-left: 40px;
-}
-.article-content{
-  padding: 0 40px;
-  margin-top: 30px;
-}
-.article-content .list{
-  height: 60px;
-  line-height: 60px;
-  border-bottom: 1px solid #E3CFB7;
-  color: #999;
-  box-sizing: border-box;
-  cursor: pointer;
-}
-.article-content .list.active{
-  color: #E3CFB7;
-}
-.article-content .list a{
-  display: block;
-  height: 100%;
-  color: inherit;
-}
-.article-content .list a:focus{
-  color: inherit;
-}
-.article-content .list a::after{
-  display: block;
-  content: '.';
-  width: 0;
-  height: 0;
-  opacity: 0;
-  visibility: hidden;
-  clear: both;
-}
+  .content {
+    padding-left: 60px;
+    padding-right: 60px;
+    border: 1px solid rgba(215, 215, 215, 1);
+  }
 
-.list-text{
-  width: 60%;
-  float: left;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-.release-time{
-  width: 10%;
-  height: 50px;
-  line-height: 76px;
-  font-size: 14px;
-  float: right;
-}
+  .article-title {
+    height: 90px;
+    line-height: 100px;
+    font-size: 28px;
+    color: #8b766c;
+    border-bottom: 1px solid #8B766C;
+    padding-left: 40px;
+  }
 
-.page-box{
-  height: 40px;
-  text-align: center;
-  margin: 80px auto;
+  .article-content {
+    padding: 0 40px;
+    margin-top: 30px;
+  }
 
-}
+  .article-content .list {
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 1px solid #E3CFB7;
+    color: #999;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
 
-.el-pagination /deep/ .btn-next, .el-pagination /deep/ .btn-prev{
-  background-color: #fff !important;
-}
+  .article-content .list.active {
+    color: #E3CFB7;
+  }
 
+  .article-content .list a {
+    display: block;
+    height: 100%;
+    color: inherit;
+  }
+
+  .article-content .list a:focus {
+    color: inherit;
+  }
+
+  .article-content .list a::after {
+    display: block;
+    content: '.';
+    width: 0;
+    height: 0;
+    opacity: 0;
+    visibility: hidden;
+    clear: both;
+  }
+
+  .list-text {
+    width: 60%;
+    float: left;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .release-time {
+    width: 10%;
+    height: 50px;
+    line-height: 76px;
+    font-size: 14px;
+    float: right;
+  }
+
+  .page-box {
+    height: 40px;
+    text-align: center;
+    margin: 80px auto;
+
+  }
+
+  .el-pagination /deep/ .btn-next,
+  .el-pagination /deep/ .btn-prev {
+    background-color: #fff !important;
+  }
 </style>
