5852a6a756c92abe55e144b25d15464c89649140
gaopeng <763429951@qq.com>
2020-04-30 17:02:40 +0800

    fix

--- web/plugins/basics.js
+++ web/plugins/basics.js
@@ -5,74 +5,84 @@ const definition = require('@/assets/js/definition.js').default
 const i18n = require('@/assets/i18n/index.js').getLang
 
 export default ({ req, app, store, $axios }, inject) => {
-  // 设置语言
-  let language = 'en_US'
-  if (isServer) {
-    if (req && req.headers && req.headers.cookie) {
-      const cookie = cookieparser.parse(req.headers.cookie || '')
-      language = cookie.language || language
+    // 设置语言
+    let language = 'en_US'
+    if (isServer) {
+        let host = req.headers['host']
+        if ((/^(cn|wap-cn)\./is).test(host)) {
+            language = 'zh_CN'
+            coin = 'HKD'
+            areaId = 1
+        } else if ((/^(us|wap-us)\./is).test(host)) {
+            language = 'en_US'
+            coin = 'USD'
+            areaId = 99
+        } else {
+            language = 'zh_TW'
+            coin = 'HKD'
+            areaId = 2
+        }
+    } else if (store && store.state) {
+        language = store.state.language
     }
-  } else if (store && store.state) {
-    language = store.state.language
-  }
-  inject('userLanguage', language)
-  inject('bddLanguage', i18n(language))
+    inject('userLanguage', language)
+    inject('bddLanguage', i18n(language))
 
-  //  设置全局搜索条件配置定义
-  inject('bddDefinition', definition(language))
+    //  设置全局搜索条件配置定义
+    inject('bddDefinition', definition(language))
 
-  inject('STATIC_PATH', process.env.STATIC_PATH)
+    inject('STATIC_PATH', process.env.STATIC_PATH)
 
-  inject('getSeoInfo', type => {
-    return $axios({
-      method: `get`,
-      url: `web/common/seo/index`,
-      params: {
-        type
-      }
-    })
-      .then(res => {
-        var data = res.data
-        return {
-          title: data.meta_title,
-          meta: [
-            {
-              name: 'title',
-              content: data.meta_title
-            },
-            {
-              name: 'description',
-              content: data.meta_desc
-            },
-            {
-              name: 'keywords',
-              content: data.meta_word
+    inject('getSeoInfo', type => {
+        return $axios({
+            method: `get`,
+            url: `web/common/seo/index`,
+            params: {
+                type
             }
-          ]
-        }
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  })
+        })
+            .then(res => {
+                var data = res.data
+                return {
+                    title: data.meta_title,
+                    meta: [
+                        {
+                            name: 'title',
+                            content: data.meta_title
+                        },
+                        {
+                            name: 'description',
+                            content: data.meta_desc
+                        },
+                        {
+                            name: 'keywords',
+                            content: data.meta_word
+                        }
+                    ]
+                }
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
 
-  inject('getDetailSeoInfo', detail => {
-    return {
-      title: detail.metaTitle || '',
-      meta: [
-        {
-          name: 'title',
-          content: detail.metaTitle || ''
-        },
-        {
-          name: 'description',
-          content: detail.metaDesc || ''
-        },
-        {
-          name: 'keywords',
-          content: detail.metaWord || ''
+    inject('getDetailSeoInfo', detail => {
+        return {
+            title: detail.metaTitle || '',
+            meta: [
+                {
+                    name: 'title',
+                    content: detail.metaTitle || ''
+                },
+                {
+                    name: 'description',
+                    content: detail.metaDesc || ''
+                },
+                {
+                    name: 'keywords',
+                    content: detail.metaWord || ''
+                }
+            ]
         }
-      ]
-    }
-  })
+    })
 }
