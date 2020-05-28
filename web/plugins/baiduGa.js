119cbb2e4a2812027f6f43ae6ce912cfba73094e
yzz <2419349086@qq.com>
2020-05-28 09:39:32 +0800

    统计代码

--- web/plugins/baiduGa.js
+++ web/plugins/baiduGa.js
@@ -2,8 +2,8 @@
 export default ({app: {router}, store}) => {
     // console.log("baidu",store.state)
     /* 每次路由变更时进行pv统计 */
-    if(store.state.platform == 20){
-        console.log(111111)
+    // if(store.state.platform == 20){
+        // console.log(111111)
         router.afterEach((to, from) => {
           /* 告诉增加一个PV */
           try {
@@ -12,5 +12,5 @@ export default ({app: {router}, store}) => {
           } catch (e) {
           }
         })
-    }
+    // }
   }
