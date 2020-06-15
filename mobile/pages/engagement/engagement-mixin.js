d1100f7aa527478fbcade0d3f145aebe63e76a75
yzz <2419349086@qq.com>
2020-06-15 11:46:56 +0800

    搜索商品详情没有折扣

--- mobile/pages/engagement/engagement-mixin.js
+++ mobile/pages/engagement/engagement-mixin.js
@@ -56,6 +56,7 @@ export default {
       chooseCarat: this.goodInfo.carats[0].content,
       chooseCaratId:this.goodInfo.carats[0].sortBy,
       showPi: this.goodInfo.salePrice,
+      showP2: 0,
       sendGoodsId: null,
       sendDetailsId: null,
       categoryId: null,
@@ -81,6 +82,11 @@ export default {
     }
   },
   mounted() {
+    if(this.couponType(this.goodInfo.coupon) == 'discount'){
+      this.showP2 = this.goodInfo.coupon.discount.price
+    }else{
+      this.showP2 = this.goodInfo.salePrice
+    }
     // this.$axios
     //   .get(`/wap/goodsComments/getAvgLevel`, {
     //     params: {
@@ -154,6 +160,12 @@ export default {
       if (this.goodInfo.carats  && this.goodInfo.sizes) {
         if (this.chooseSizeId === `` || this.chooseCaratId === ``) {
           this.showPi = this.goodInfo.salePrice
+
+          if(this.couponType(this.goodInfo.coupon) == 'discount'){
+            this.showP2 = this.goodInfo.coupon.discount.price
+          }else{
+            this.showP2 = this.goodInfo.salePrice
+          }
         } else {
           for (const i in bullShit) {
             if (
@@ -166,12 +178,24 @@ export default {
               this.sendGoodsId = bullShit[i].goodsId
               this.sendDetailsId = bullShit[i].id
               this.categoryId = bullShit[i].categoryId
+
+              if(this.couponType(bullShit[i].coupon) == 'discount'){
+                this.showP2 = bullShit[i].coupon.discount.price
+              }else{
+                this.showP2 = bullShit[i].retailMallPrice
+              }
             }
           }
         }
       } else {
         if (this.chooseSizeId === ``) {
           this.showPi = this.goodInfo.salePrice
+
+          if(this.couponType(this.goodInfo.coupon) == 'discount'){
+            this.showP2 = this.goodInfo.coupon.discount.price
+          }else{
+            this.showP2 = this.goodInfo.salePrice
+          }
         } else {
           for (const i in bullShit) {
             if (
@@ -183,6 +207,12 @@ export default {
               this.sendGoodsId = bullShit[i].goodsId
               this.sendDetailsId = bullShit[i].id
               this.categoryId = bullShit[i].categoryId
+
+              if(this.couponType(bullShit[i].coupon) == 'discount'){
+                this.showP2 = bullShit[i].coupon.discount.price
+              }else{
+                this.showP2 = bullShit[i].retailMallPrice
+              }
             }
           }
         }
