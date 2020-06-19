0b2eb106c5e396875ad310c9d9f1cd5d1bb48f01
donsilor <390807392@qq.com>
2020-06-19 18:29:43 +0800

    updata

---
+++ mobile/pages/generalize/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <img src="../../static/generalize/img_01.jpg" alt="">
+    <img src="../../static/generalize/img_02.jpg" alt="">
+    <img src="../../static/generalize/img_03.jpg" alt="">
+    <img src="../../static/generalize/img_04.jpg" alt="">
+    <img src="../../static/generalize/img_05.jpg" alt="">
+    <img src="../../static/generalize/img_06.jpg" alt="">
+    <img src="../../static/generalize/img_07.jpg" alt="">
+    <img src="../../static/generalize/img_08.jpg" alt="">
+    <img src="../../static/generalize/img_09.jpg" alt="">
+    <img src="../../static/generalize/img_10.jpg" alt="">
+    <img src="../../static/generalize/img_11.jpg" alt="">
+    <img src="../../static/generalize/img_12.jpg" alt="">
+    <img src="../../static/generalize/img_13.jpg" alt="">
+    <img src="../../static/generalize/img_14.jpg" alt="">
+    <img src="../../static/generalize/img_15.jpg" alt="">
+    <img src="../../static/generalize/img_16.jpg" alt="">
+    <img src="../../static/generalize/img_17.jpg" alt="">
+    <img src="../../static/generalize/img_18.jpg" alt="">
+    <img src="../../static/generalize/img_19.jpg" alt="">
+    <img src="../../static/generalize/img_20.jpg" alt="">
+    <img src="../../static/generalize/img_21.jpg" alt="">
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  img{
+    width: 100%;
+  }
+</style>
