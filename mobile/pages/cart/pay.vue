5f221144f348601fd9041e00a393514b96ac3216
yzz <2419349086@qq.com>
2020-08-04 15:52:49 +0800

    yyy

--- mobile/pages/cart/pay.vue
+++ mobile/pages/cart/pay.vue
@@ -654,7 +654,7 @@ export default {
                 },
                 function(res){
                 if(res.err_msg == "get_brand_wcpay_request:ok" ){
-                  //  window.location.replace(encodeURIComponent(baseUrl+'/complete/paySuccess?orderId='+this.info.orderId))
+                   window.location.replace(baseUrl+'/complete/paySuccess?orderId='+this.info.orderId)
                 // 使用以上方式判断前端返回,微信团队郑重提示：
                       //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
                 } 
