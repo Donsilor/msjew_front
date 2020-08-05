51e64098523a4bba824501affdd3fc75f21828d5
yzz <2419349086@qq.com>
2020-08-05 11:17:00 +0800

    移动微信支付

--- mobile/pages/cart/pay.vue
+++ mobile/pages/cart/pay.vue
@@ -655,7 +655,7 @@ export default {
                   "paySign":res.paySign //微信签名 
                 },
                 function(url){
-                  alert(url)
+                  // alert(url)
                   // alert(res.err_msg)
                   // if(res.err_msg == "get_brand_wcpay_request:ok" ){
                   //   alert(11111111111)
@@ -666,8 +666,8 @@ export default {
                   // }
                   return function(res) {
                     if(res.err_msg == "get_brand_wcpay_request:ok" ){
-                      alert(11111111111)
-                      alert(url)
+                      // alert(11111111111)
+                      // alert(url)
                       window.location.replace(url)
                       // 使用以上方式判断前端返回,微信团队郑重提示：
                       //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
