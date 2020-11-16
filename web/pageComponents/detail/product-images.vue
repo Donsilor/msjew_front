cc15641965ce14a47a338face195233cbb8c665c
donsilor <390807392@qq.com>
2020-11-16 12:29:23 +0800

    updata

--- web/pageComponents/detail/product-images.vue
+++ web/pageComponents/detail/product-images.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="product-images">
-    <ul class="thumbnails">
-      <li v-if="goods3ds" class="item">
-        <img src="/diamonds/360.png" />
+    <div class="thumbnails">
+      <li v-if="maxLength > 0" class="img-btn btn-up" @click="upward()">
+        <i class="el-icon el-icon-arrow-up" :class="{'non': showIndex == 0}"></i>
       </li>
-      <li
-        v-for="(item, index) in images"
-        :key="index"
-        :class="['item', { active: index === activeIndex }]"
-        @click="changeActiveIndex(index)"
-      >
-        <img :src="item" />
+      
+      <div class="scroll-wrap">
+        <ul class="scroll">
+          <li v-if="goods3ds" class="item">
+            <img src="/diamonds/360.png" />
+          </li>
+          <li
+            v-for="(item, index) in images"
+            :key="index"
+            :class="['item', { active: index === activeIndex }]"
+            @click="changeActiveIndex(index)"
+          >
+            <img :src="item" />
+          </li>
+          <li v-if="giaNumber" class="item">
+            <a
+              :href="'https://www.gia.edu/report-check?reportno=' + giaNumber"
+              target="_blank"
+            >
+              <img src="/diamonds/gia.png" />
+            </a>
+          </li>
+          <li v-if="showCode" class="item hand" @mouseover="showEwm = true, getEwm()" @mouseout="showEwm = false">
+            <img src="/icon/hand.png" />
+          </li>
+        </ul>
+      </div>
+
+      <li v-if="maxLength > 0" class="img-btn btn-down" @click="downward()">
+        <i class="el-icon el-icon-arrow-down" :class="{'non': showIndex >= (maxLength-1)}"></i>
       </li>
-      <li v-if="giaNumber" class="item">
-        <a
-          :href="'https://www.gia.edu/report-check?reportno=' + giaNumber"
-          target="_blank"
-        >
-          <img src="/diamonds/gia.png" />
-        </a>
-      </li>
-      <li v-if="showCode" class="item hand" @mouseover="showEwm = true, getEwm()" @mouseout="showEwm = false">
-        <img src="/icon/hand.png" />
-      </li>
-    </ul>
+    </div>
 
     <div class="main-pic">
       <img :src="images[activeIndex]" />
@@ -99,6 +111,7 @@ export default {
       language: this.$store.state.language,
       qrcodeObj: {}, // 二维码配置
       showEwm: false, //是否显示二维码弹窗
+      showIndex: 0
     }
   },
   computed: {
@@ -107,13 +120,17 @@ export default {
     },
     showCode(){
       return this.ifCode
-    }
-  },
-  mounted() {
-    const _this = this
-    _this.$nextTick(() => {})
+    },
+    maxLength() {
+      var max;
+      if(!!this.showCode){
+        max = (+this.images.length + 1)-5
+      }else{
+        max = this.images.length-5
+      }
 
-    // this.language = this.getCookie('language')
+      return max
+    }
   },
   methods: {
     changeActiveIndex(index) {
@@ -134,43 +151,18 @@ export default {
 
       let headerHost = location.host;
       //生产环境
-      let host = 'https://wap.bddco.com';
-      if ((/^(cn|us|tw)\.bddco\.com/).test(headerHost)) {
+      let host = 'https://wap.msjew.com';
+      if ((/^(hk|us|tw)\.msjew\.com/).test(headerHost)) {
           //生产环境
           host = 'https://wap-' + headerHost;
-      } else if ((/bddia\.com/).test(headerHost)) {
-          //测试环境 bddia.com
-          host = 'https://wap.bddia.com';
-          if ((/^(cn|us)\.bddia\.com/).test(headerHost)) {
-              host = 'https://wap-' + headerHost;
-          }
       } else if ((/bddco.cn/).test(headerHost)) {
-          //大陆站点 bddco.cn
-          host = 'https://wap.bddco.cn'
-          if ((/^(cn|us|tw)-bdd.bddco.cn/).test(headerHost)) {
+          
+          if ((/^(hk|us|tw)-msjew.bddco.cn/).test(headerHost)) {
               host = 'https://wap-' + headerHost;
-          } else if (headerHost == 'www-bdd.bddco.cn') {
-              host = 'https://wap-bdd.bddco.cn';
+          } else if (headerHost == 'www-msjew.bddco.cn') {
+              host = 'https://wap-msjew.bddco.cn'
           }
       }
-
-
-      // if ((/^(hk|hk-msjew|wap-hk)\./is).test(host)) {
-      //       language = 'zh_TW'
-      //       coin = 'HKD'
-      //       areaId = 2
-      //       platform = 10 //PC香港
-      //   } else if ((/^(us|us-msjew|wap-us)\./is).test(host)) {
-      //       language = 'en_US'
-      //       coin = 'USD'
-      //       areaId = 99
-      //       platform = 30 //PC美国
-      //   } else if ((/^(tw|tw-msjew|wap-tw)\./is).test(host)) {
-      //       language = 'zh_TW'
-      //       coin = 'TWD'
-      //       areaId = 3
-      //       platform = 40 //PC台湾
-      //   } else {
       
       var url = host + path;
 
@@ -184,6 +176,30 @@ export default {
         colorLight : '#fff',
         correctLevel : QRCode.CorrectLevel.H
       })
+    },
+    upward() {
+      var max = this.images.length-5-1;
+      if(!!this.showCode){
+        max+=1
+      }
+      if(this.showIndex <= max && this.showIndex > 0){
+        var scroll = document.getElementsByClassName('scroll')[0];
+        this.showIndex--;
+        scroll.style.top = this.showIndex * -85 + 'px';
+        scroll.style.transition = 'top .2s ease-in';
+      }
+    },
+    downward() {
+      var max = this.images.length-5-1;
+      if(!!this.showCode){
+        max+=1
+      }
+      if(this.showIndex < max && this.showIndex >= 0){
+        var scroll = document.getElementsByClassName('scroll')[0];
+        this.showIndex++;
+        scroll.style.top = this.showIndex * -85 + 'px';
+        scroll.style.transition = 'top .2s ease-in';
+      }
     }
   }
 }
@@ -194,44 +210,81 @@ export default {
   display: flex;
 
   .thumbnails {
-    max-height: 500px;
-    overflow: auto;
+    height: 500px;
     list-style: none;
     margin-right: 33px;
     display: flex;
     flex-direction: column;
+    position: relative;
 
     &::-webkit-scrollbar {
       width: 0;
       height: 0;
     }
 
-    .item {
-      flex-grow: 0;
-      flex-shrink: 0;
-      position: relative;
-      margin-bottom: 13px;
+    .scroll-wrap{
       width: 72px;
-      height: 72px;
-      border: 1px solid #dddddd;
-      box-sizing: border-box;
-      font-size: 0;
-      line-height: 0;
+      height: 100%;
+      position: relative;
       overflow: hidden;
-      transition: all 0.1s linear;
 
-      &.active {
-        border: 1px solid rgba(170, 138, 123, 1);
-      }
-
-      img {
+      .scroll{
         position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        max-width: 95%;
-        max-height: 95%;
+        top: 0;
+        left: 0;
+        width: 100%;
+
+        .item {
+          flex-grow: 0;
+          flex-shrink: 0;
+          position: relative;
+          margin-bottom: 13px;
+          width: 72px;
+          height: 72px;
+          border: 1px solid #dddddd;
+          box-sizing: border-box;
+          font-size: 0;
+          line-height: 0;
+          overflow: hidden;
+          transition: all 0.1s linear;
+
+          &.active {
+            border: 1px solid rgba(170, 138, 123, 1);
+          }
+
+          img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            max-width: 95%;
+            max-height: 95%;
+          }
+        }
       }
+    } 
+
+    .img-btn{
+      position: absolute;
+      left: 50%;
+      top: -30px;
+      transform: translateX(-50%);
+      width: 24px;
+      height: 20px;
+      cursor: pointer;
+      text-align: center;
+      color: #444;
+      font-size: 18px;
+
+      .non{
+        color: #ccc;
+        cursor: initial;
+      }
+    }
+
+    .btn-down{
+      top: initial;
+      bottom: -30px;
     }
   }
 
@@ -239,8 +292,7 @@ export default {
     position: relative;
     width: 500px;
     height: 500px;
-    border: 1px solid #dddddd;
-    box-sizing: border-box;
+    border: 1px solid #ddd;
     font-size: 0;
     line-height: 0;
     // overflow: hidden;
